cb13242216ea5704eb609890024adf6f33674281
4np <work@osx.eu>
2011-08-12 08:27:41 +0000

    - added files so the plugin will be able to run stand-alone (for development purposes)

--- UploadrGrailsPlugin.groovy
+++ UploadrGrailsPlugin.groovy
@@ -19,7 +19,7 @@
  *  $Date: 2011-08-11 14:04:06 +0200 (Thu, 11 Aug 2011) $
  */
 class UploadrGrailsPlugin {
-	def version			= "0.4.1"
+	def version			= "0.4.2"
 	def grailsVersion	= "1.3.7 > *"
 	def dependsOn		= [jquery: "1.4 => *", jqueryUi: "1.8 => *", modernizr: "1.7.2 => *", resources: "1.0 => *", famfamfam: "1.0 => *"]
 	def pluginExcludes	= [
